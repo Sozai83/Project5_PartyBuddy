f4056643e3c4750956c9bfc6bec3199a7fc88ac7
Sozai83 <chikushi3.227@gmail.com>
2023-07-15 21:11:53 +1000

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Party Buddy',
+  description: 'A web application you can help you decide what cocktail you can make for the party tonight!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
