a1b18a6003cadf8b0e82419e65021faa847ae1d1
Sozai83 <chikushi3.227@gmail.com>
2023-07-16 22:23:47 +1000

    added options component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.scss'
+import Options from './components/Options'
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <h1>Bar Buddy</h1>
         <h2>We help you pick a cocktail tonight</h2>
       </section>
+      <Options/>
     </main>
   )
 }
