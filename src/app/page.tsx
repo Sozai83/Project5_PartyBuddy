26499d3e5f528cbe019207828712e82b5204a575
Sozai83 <chikushi3.227@gmail.com>
2023-07-16 21:55:19 +1000

    added top logo and the title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,16 @@ import styles from './page.module.scss'
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <section className={styles.logo}>
+        <Image 
+          src="/favicon.png"
+          width={500}
+          height={500}
+          alt="Party Buddy Logo"
+        />
+        <h1>Bar Buddy</h1>
+        <h2>We help you pick a cocktail tonight</h2>
+      </section>
     </main>
   )
 }
