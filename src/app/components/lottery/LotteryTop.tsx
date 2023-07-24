fbf309fe42a6a2e841f940b82282aa35d3186df8
Sozai83 <chikushi3.227@gmail.com>
2023-07-24 23:53:30 +1000

    added recipe page

--- src/app/components/lottery/LotteryTop.tsx
+++ src/app/components/lottery/LotteryTop.tsx
@@ -2,13 +2,16 @@ import Image from 'next/image'
 import Lottery from './lottery/Lottery'
 import { useState } from 'react';
 import Result from './result/Result';
+import Recipe from './recipe/Recipe';
 
 
 export default function LotteryTop() {
     const [result, setResult] = useState(true);
-    const [showRecipe, setShowRecipe] = useState();
+    const [showRecipe, setShowRecipe] = useState(true);
 
     return (
-        result ? <Result /> : <Lottery />
+        result && !showRecipe ? <Result /> :
+            showRecipe ? <Recipe /> :
+                <Lottery />
     )
 }
---
+++ src/app/components/lottery/recipe/Recipe.tsx
@@ -0,0 +1,62 @@
+import styles from './Recipe.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRotateRight } from '@fortawesome/free-solid-svg-icons'
+import Image from 'next/image'
+
+export default function Result() {
+    return (
+        <section className={styles.recipeWrapper}>
+            <section className={styles.top}>
+                <h1>Cocktail Name</h1>
+            </section>
+            <section className={styles.ingredients}>
+                <figure className={styles.resultImg}>
+                    <Image
+                        src="/img/cocktail/sample_cocktail.jpg"
+                        width={300}
+                        height={300}
+                        alt="Sample Cocktail"
+                    />
+                </figure>
+                <section className={styles.ingredientsDesc}>
+                    <h2>Ingredients <span>Serving: X</span></h2>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Vodka</td>
+                                <td>1 oz</td>
+                            </tr>
+                            <tr>
+                                <td>Vodka</td>
+                                <td>1 oz</td>
+                            </tr>
+                            <tr>
+                                <td>Vodka</td>
+                                <td>1 oz</td>
+                            </tr>
+                            <tr>
+                                <td>Vodka</td>
+                                <td>1 oz</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
+            </section>
+
+
+            <section className={styles.recipe}>
+                <ul>
+                    <li>
+                        step1
+                    </li>
+                    <li>
+                        step2
+                    </li>
+                    <li>
+                        step3
+                    </li>
+                </ul>
+            </section>
+        </section>
+    )
+}
