90ad2526bb3906819685fbb117e5c8e2e7b8ac90
Sozai83 <chikushi3.227@gmail.com>
2023-07-24 22:57:05 +1000

    added result page

--- src/app/components/lottery/LotteryTop.tsx
+++ src/app/components/lottery/LotteryTop.tsx
@@ -1,9 +1,14 @@
 import Image from 'next/image'
 import Lottery from './lottery/Lottery'
+import { useState } from 'react';
+import Result from './result/Result';
 
 
 export default function LotteryTop() {
+    const [result, setResult] = useState(true);
+    const [showRecipe, setShowRecipe] = useState();
+
     return (
-        <Lottery />
+        result ? <Result /> : <Lottery />
     )
 }
--- src/app/components/lottery/result/Result.tsx
+++ src/app/components/lottery/result/Result.tsx
@@ -3,20 +3,19 @@ import Image from 'next/image'
 
 export default function Result() {
     return (
-        <section className={styles.optionWrapper}>
-            <h1>Let us decide what you're gonna have tonight!</h1>
-            <figure>
+        <section className={styles.resultWrapper}>
+            <figure className={styles.resultImg}>
                 <Image
-                    src="/bartender.jpg"
+                    src="/img/cocktail/sample_cocktail.jpg"
                     width={400}
                     height={400}
-                    alt="Party Buddy Logo"
+                    alt="Sample Cocktail"
                 />
-                <sup><a href="https://www.freepik.com/free-vector/cocktail-bartender-concept-illustration_25026369.htm#query=illustrations%20bartender&position=1&from_view=search&track=ais">Image by storyset</a> on Freepik</sup>
             </figure>
-            <section className={styles.buttons}>
-                <button className={styles.filtering}>Filtering</button>
-                <button className={styles.lottery}>Pick Cocktail</button>
+            <section className={styles.resultDesc}>
+                <h1>Cocktail Name</h1>
+                <p>Description</p>
+                <button className={styles.tryAgain}>See Recipe</button>
             </section>
         </section>
     )
