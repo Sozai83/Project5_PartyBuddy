9a960a595b1b988fee2852c8a6aa03cda521d156
Sozai83 <chikushi3.227@gmail.com>
2023-08-04 23:09:37 +1000

    added API request - search cocktails by ingredients

--- src/app/api/api.tsx
+++ src/app/api/api.tsx
@@ -1,5 +1,7 @@
 //Look up a random cocktail - https://www.thecocktaildb.com/api.php 
 const randomCocktailAPI = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+const filterCocktailsByIngredients = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i='
+
 export const pickCocktail = async (setResult: any) => {
     await fetch(randomCocktailAPI, { cache: 'no-store' })
         .then(cocktailData => {
@@ -8,3 +10,14 @@ export const pickCocktail = async (setResult: any) => {
             jsonCocktailData => setResult(jsonCocktailData.drinks[0])
         );
 }
+
+const filterCocktail = async (ingredient: string, setFilter: any) => {
+    const apiURL = filterCocktailsByIngredients + ingredient;
+    await fetch(apiURL, { cache: 'no-store' })
+        .then(filteredData => {
+            return filteredData.json();
+        }).then(
+            jsonFilteredData => jsonFilteredData.drinks
+        );
+}
+
