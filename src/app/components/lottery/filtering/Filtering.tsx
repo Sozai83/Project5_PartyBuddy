eb8f4b5dea89a7c400bf8fd90451a5e03e50d6a2
Sozai83 <chikushi3.227@gmail.com>
2023-08-04 23:02:38 +1000

    added more options for liquor

--- src/app/components/lottery/filtering/Filtering.tsx
+++ src/app/components/lottery/filtering/Filtering.tsx
@@ -7,16 +7,32 @@ export default function Filtering({ setResult }: any) {
         <section className={styles.filteringWrapper}>
             <form>
                 <div className={styles.radio}>
-                    <input type="radio" id="html" name="alcohol" value="HTML" />
-                    <label htmlFor="html">HTML</label>
-                </div>
-                <div className={styles.radio}>
-                    <input type="radio" id="gin" name="alcohol" value="gin" />
+                    <input type="radio" id="gin" name="alcohol" value="Gin" />
                     <label htmlFor="gin">Gin</label>
                 </div>
                 <div className={styles.radio}>
-                    <input type="radio" id="vodka" name="alcohol" value="vodka" />
+                    <input type="radio" id="vodka" name="alcohol" value="Vodka" />
                     <label htmlFor="vodka">Vodka</label>
+                </div>
+                <div className={styles.radio}>
+                    <input type="radio" id="tequila" name="alcohol" value="Tequila" />
+                    <label htmlFor="tequila">Tequila</label>
+                </div>
+                <div className={styles.radio}>
+                    <input type="radio" id="whiskey" name="alcohol" value="Whiskey" />
+                    <label htmlFor="whiskey">Whiskey</label>
+                </div>
+                <div className={styles.radio}>
+                    <input type="radio" id="rum" name="alcohol" value="Rum" />
+                    <label htmlFor="rum">Rum</label>
+                </div>
+                <div className={styles.radio}>
+                    <input type="radio" id="bourbon" name="alcohol" value="Bourbon" />
+                    <label htmlFor="bourbon">Bourbon</label>
+                </div>
+                <div className={styles.radio}>
+                    <input type="radio" id="scotch" name="alcohol" value="Scotch" />
+                    <label htmlFor="scotch">Scotch</label>
                 </div>
             </form>
         </section>
